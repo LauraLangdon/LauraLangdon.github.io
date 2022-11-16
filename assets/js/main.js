77364b439c00db54e5a159ab26f1fa8a42ef5db3
Laura Langdon <48335772+LauraLangdon@users.noreply.github.com>
2022-11-16 12:34:08 -0800

    a11y upgrades

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@
 		// Title Bar.
 			$(
 				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
+					'<a href="#navPanel" class="toggle">Navigation</a>' +
 				'</div>'
 			)
 				.appendTo($body);
