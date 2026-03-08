7cbd506a38986bbde9ba93703317328b96a0d9c5
Laura Langdon <48335772+LauraLangdon@users.noreply.github.com>
2026-03-08 16:36:49 -0700

    Fully move to local md files with Astro

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,10 +2,13 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://staging.lauralangdon.io',
 	integrations: [mdx(), sitemap()],
+	image: {
+		service: sharpImageService({ limitInputPixels: false }),
+	},
 });
--- frontend/src/content.config.ts
+++ frontend/src/content.config.ts
@@ -2,18 +2,18 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-		}),
+	loader: glob({ base: './posts', pattern: '*/index.md' }),
+	schema: z.object({
+		title: z.string(),
+		slug: z.string(),
+		status: z.enum(['published', 'draft']).default('published'),
+		featured: z.boolean().default(false),
+		date: z.coerce.date(),
+		tags: z.array(z.string()).optional(),
+		excerpt: z.string().optional(),
+		feature_image: z.string().optional(),
+		feature_image_alt: z.string().optional(),
+	}),
 });
 
 export const collections = { blog };
